32fb21393f2d7a7da3bd2c79da8c5f1b422d61b1
lichengjie <lichengjie@wdai.com>
2018-10-30 21:05:01 +0800

    提交flux

--- src/dispatcher/AppDispatcher.js
+++ src/dispatcher/AppDispatcher.js
@@ -6,7 +6,7 @@ import ListStore from '../stores/ListStore'
 var AppDispatcher=new Dispatcher();
 
 
-AppDispatcher.register(function (action) {
+AppDispatcher.register(function (action) {  //分配器对应的事件
   switch(action.actionType) {
     case 'ADD_NEW_ITEM':
       ListStore.addNewItemHandler(action.text);
--- src/stores/ListStore.jsx
+++ src/stores/ListStore.jsx
@@ -1,25 +1,25 @@
 import {EventEmitter} from 'events'
 import objectAssign from 'object-assign'
 
-var ListStore=objectAssign({},EventEmitter.prototype,{
+var ListStore=objectAssign({},EventEmitter.prototype,{ //继承EventEmitter.prototype事件监听
   items: [],
 
-  getAll: function() {
+  getAll: function() {   //拿到全部数据
     return this.items;
   },
 
-  addNewItemHandler: function (text) {
+  addNewItemHandler: function (text) { //实际修改数据
     this.items.push(text);
   },
 
-  emitChange: function () {
+  emitChange: function () { //触发change事件
     this.emit('change');
   },
-  addChangeListener: function(callback) {
+  addChangeListener: function(callback) { //添加绑定监听事件
     this.on('change', callback);
   },
 
-  removeChangeListener: function(callback) {
+  removeChangeListener: function(callback) {  //移除绑定监听事件
     this.removeListener('change', callback);
   }
 })
